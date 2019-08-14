d0e0a739cd718c87f805e5565addada7cb14b8bf
nyan <nyan969@ya.ru>
2019-08-14 21:56:29 +0300

    Элементы перечислений переименованны капсом

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,4 @@
 export const API = {
-    token: '35f83a00-b4b7-4349-805e-b09f348c0ad8',
-    url: 'https://praktikum.tk/cohort0'
+    TOKEN: '35f83a00-b4b7-4349-805e-b09f348c0ad8',
+    URL: 'https://praktikum.tk/cohort0'
 };
